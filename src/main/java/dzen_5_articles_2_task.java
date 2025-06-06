a982cf6974f2d194ccb8561ed79ff2f8e0018ebe
aurum <aurum727@mail.ru>
2025-06-06 14:51:38 +0300

    2_module_task_dzen_5_articles

---
+++ src/main/java/dzen_5_articles_2_task.java
@@ -0,0 +1,24 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
+import static com.codeborne.selenide.Selenide.*;
+
+public class dzen_5_articles_2_task {
+
+    public static void main(String[] args) {
+        Configuration.browser = "chrome";
+        Configuration.headless = false; // false - чтобы видеть браузер
+        Configuration.timeout = 10000; // 10 секунд ожидания элементов
+//        Configuration.browserSize = "1920x1080"; // Размер окна браузера
+        open("https://www.dzen.ru");
+
+        ElementsCollection news = $$x("//li[@data-testid=\"news-item\"]//p[@aria-label=\"Заголовок\"]");
+        int count = news.size(); // extract to evaluate once only
+        for (int i=0; i<count; i++) {
+            if (news.get(i).getText() != "") {
+                System.out.println(news.get(i).getText());
+            }
+        }
+        closeWebDriver();
+    }
+
+}
